8e262b106ccb2455a20d3b555dd72d26ed5a11ff
pisfe <pedroicaro@outlook.com>
2025-08-09 09:49:59 -0300

    aula 02-01 continuação formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Formulario.css';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front',
+    'Data Science',
+    'DevOps',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ];
+
   return (
     <section className='formulario'>
         <form>
@@ -12,6 +25,8 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite seu nome"></CampoTexto>
             <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
             <CampoTexto label="Imagem" placeholder="Digite seu imagem"></CampoTexto>
+            <ListaSuspensa itens={times}></ListaSuspensa>
+            <Botao>Criar card</Botao>
         </form>
     </section>
   );
